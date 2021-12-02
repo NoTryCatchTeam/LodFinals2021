217292fb9f93b3a38b2c65356af73be650689eee
Сергей Колчин <swamp1820@outlook.com>
2021-12-02 23:45:46 +0300

    fix swagger

--- LODFinals.Api/Startup.cs
+++ LODFinals.Api/Startup.cs
@@ -40,10 +40,11 @@ namespace LODFinals.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LODFinals.Api v1"));
+              
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LODFinals.Api v1"));
             //app.UseHttpsRedirection();
 
             app.UseRouting();
