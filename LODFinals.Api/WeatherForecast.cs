073f8e4ac4112814b6a10099952dbcf79a7595b1
Сергей Колчин <swamp1820@outlook.com>
2021-12-02 17:47:43 +0300

    заменил API

--- LODFinals.Api/WeatherForecast.cs
+++ LODFinals.Api/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace LODFinals.Api
 {
     public class WeatherForecast
@@ -8,6 +10,6 @@ namespace LODFinals.Api
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
