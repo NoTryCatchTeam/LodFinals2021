f49d563b7334510bb8831464539e8c8f5aed4e46
Dmitry Ordenov <dordenov@artec3d.com>
2021-12-03 11:54:04 +0300

    added some animations

--- LODFinals/wwwroot/js/main.js
+++ LODFinals/wwwroot/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
 
+
 });
 
 function initIndex() {
     VanillaTilt.init(document.querySelectorAll(".analytics-item"));
+
+    $(".notifications").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function (e) {
+        $(".notifications img").removeClass("animate__swing")
+    }).mouseenter(function (e) {
+        $(".notifications img").addClass("animate__swing");
+    });
 }
