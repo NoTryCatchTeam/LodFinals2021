6c482ff2c88c009c6a708eeeff41d16ea8e4c98d
Dmitry Ordenov <dordenov@artec3d.com>
2021-12-03 22:06:51 +0300

    finished photo ui, added animations, changed font

--- LODFinals/wwwroot/js/main.js
+++ LODFinals/wwwroot/js/main.js
@@ -6,17 +6,25 @@ function initIndex() {
     commonInit();
 
     VanillaTilt.init(document.querySelectorAll(".analytics-item"));
-
-    $(".notifications").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function (e) {
-        $(".notifications img").removeClass("animate__swing")
-    }).mouseenter(function (e) {
-        $(".notifications img").addClass("animate__swing");
-    });
 }
 
 function initLogin() {
     commonInit();
 }
 
+function initPhotos() {
+    commonInit();
+}
+
 function commonInit() {
+    
+    topBarInit();
+}
+
+function topBarInit() {
+    $(".notifications").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function (e) {
+        $(".notifications img").removeClass("animate__swing")
+    }).mouseenter(function (e) {
+        $(".notifications img").addClass("animate__swing");
+    });
 }
