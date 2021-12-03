283052c6648e6db06a1c73250d048334bdb60017
Dmitry Ordenov <dordenov@artec3d.com>
2021-12-03 12:53:21 +0300

    added some animations, moving to login page

--- LODFinals/wwwroot/js/main.js
+++ LODFinals/wwwroot/js/main.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
 
+    // $(document).on('click', 'a.nav-href', function (e) {
+    //     let a = $(this)[0];
+    //     console.log(a);
+    //
+    //     let href = a.href;
+    //     e.preventDefault();
+    //     $(".index").removeClass("animate__fadeIn").addClass("animate__fadeOut");
+    //
+    //     setTimeout(function () {
+    //             window.location.replace(href);
+    //         },
+    //         1000);
+    // });
 
 });
 
 function initIndex() {
+    commonInit();
+
     VanillaTilt.init(document.querySelectorAll(".analytics-item"));
 
     $(".notifications").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function (e) {
@@ -11,4 +26,9 @@ function initIndex() {
     }).mouseenter(function (e) {
         $(".notifications img").addClass("animate__swing");
     });
+
+
+}
+
+function commonInit() {
 }
