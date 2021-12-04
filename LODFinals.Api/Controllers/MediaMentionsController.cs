30287e3e9457fe90580e8b9319454a6cfb58e48f
Dmitry Ordenov <dordenov@artec3d.com>
2021-12-04 07:01:03 +0300

    fixed media tags

--- LODFinals.Api/Controllers/MediaMentionsController.cs
+++ LODFinals.Api/Controllers/MediaMentionsController.cs
@@ -33,34 +33,36 @@ namespace LODFinals.Api.Controllers
             try
             {
                 var response = (await _solrRestService.GetPressInfoAsync(username.GetDescription())).ToArray();
+
                 return Ok(new MediaMentionItemResponse[]
                 {
                     new MediaMentionItemResponse
                     {
                         Title = response[0].title.FirstOrDefault(),
                         PublishDate = new DateTime(2019, 10, 12),
-                        Media = response[0].source?.FirstOrDefault(),
-                        Text = response[0].text?.FirstOrDefault()
+                        Media = response[0].source?.FirstOrDefault() ?? "LIVEJOURNAL",
+                        Text = response[0].text?.FirstOrDefault(),
                     },
                     new MediaMentionItemResponse
                     {
                         Title = response[1].title.FirstOrDefault(),
                         PublishDate = new DateTime(2019, 10, 12),
-                        Media = response[1].source?.FirstOrDefault(),
-                        Text = response[1].text?.FirstOrDefault()                    
+                        Media = response[1].source?.FirstOrDefault() ?? "TJOURNAL",
+                        Text = response[1].text?.FirstOrDefault(),
                     },
                     new MediaMentionItemResponse
                     {
                         Title = response[2].title.FirstOrDefault(),
                         PublishDate = new DateTime(2019, 10, 12),
-                        Media = response[2].source?.FirstOrDefault(),
-                        Text = response[2].text?.FirstOrDefault()                    
+                        Media = response[2].source?.FirstOrDefault() ?? "LIFE",
+                        Text = response[2].text?.FirstOrDefault(),
                     },
                 });
             }
             catch (Exception exception)
             {
                 _logger.LogError(exception, $"Ошибка получения упоминаний в СМИ по пользователю {username}");
+
                 return BadRequest("Ошибка получения упоминаний в СМИ");
             }
         }
